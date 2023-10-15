a345017f765c7dd240f4bb6edcdafa74f67ea519
Youta1218 <youtan.yamamoto@gmail.com>
2023-10-15 23:48:36 +0900

    store_edit修正仮完了

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -14,9 +14,8 @@ class SeriesController extends Controller
 {
     public function series(Series $series)
     {
-        $books=$series->getBySeries();
-        $user_books = $books->whereIn("user_id", Auth::id());
-        $this->authorize('view', $user_books->first());
+        $user_books = $series->books()->where('user_id', Auth::id())->get();
+        
     return view('books.series')->with(['books' => $user_books, 'series'=>$series]);
     }   
     public function seriesps (Series $series)
